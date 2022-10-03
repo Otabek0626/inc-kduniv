08cae6608d25edeed4566057f3143ec629bf1fee
Otabek <bekdevs01@gmail.com>
2022-10-03 17:33:24 +0900

    form

--- index/models.py
+++ index/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+
+def upload_location(instance, filename):
+    filebase, extension = filename.split('.')
+    return 'static/assets/images/%s.%s' % (f'{instance.email}_{instance.first_name}', extension)
+
+
+
+class Profile(models.Model):
+    first_name = models.CharField(max_length = 50)
+    middle_name = models.CharField(max_length = 50)
+    last_name = models.CharField(max_length = 50)
+    gender = models.CharField(max_length = 50)
+    image = models.ImageField(upload_to = upload_location)
+    country = models.CharField(max_length = 50)
+    nationality = models.CharField(max_length = 50)
+    passport_number = models.CharField(max_length = 50)
+    home_address = models.CharField(max_length = 150)
+    current_address = models.CharField(max_length = 150)
+    tel_number_home = models.CharField(max_length = 50)
+    tel_number_mobile = models.CharField(max_length = 50)
+    email = models.EmailField(max_length = 100)
+    name_of_institution = models.CharField(max_length = 50)
+    address_of_institution = models.CharField(max_length = 150)
+    degree = models.CharField(max_length = 50)
+    department = models.CharField(max_length = 150)
+    major = models.CharField(max_length = 150)
+    tem_number_of_institution = models.CharField(max_length = 50)
+    email_of_institution = models.EmailField(max_length = 100)
+    english = models.CharField(max_length = 50)
+    korean = models.CharField(max_length = 50)
+    statement_of_interest = models.TextField()
+ 
+    def __str__(self):
+        return self.email
--- index/views.py
+++ index/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-
+from .models import Profile
 # Create your views here.
 def main(request, html='index'):
     try:
@@ -10,4 +10,32 @@ def main(request, html='index'):
 
 
 def form(request):
+    if request.method == "POST":
+        first_name = request.POST["firstName"]
+        middle_name = request.POST["middleName"]
+        last_name = request.POST["lastName"]
+        gender = request.POST["gender"]
+        image = request.FILES["photograph"]
+        country = request.POST["country"]
+        nationality = request.POST["nationality"]
+        passport_number = request.POST["passport"]
+        home_address = request.POST["homeAddress"]
+        current_address = request.POST["currentAddress"]
+        tel_number_home = request.POST["homeTelNumber"]
+        tel_number_mobile = request.POST["MobileTelNumber"]
+        email = request.POST["email"]
+        name_of_institution = request.POST["name_institution"]
+        address_of_institution = request.POST["address_inst"]
+        degree = request.POST["degree"]
+        department = request.POST["department"]
+        major = request.POST["major"]
+        tem_number_of_institution = request.POST["institureTel"]
+        email_of_institution = request.POST["instituteEmail"]
+        english = request.POST["proficiencyEnglish"]
+        korean = request.POST["proficiencyKorean"]
+        statement_of_interest = request.POST["Statement"]
+        
+        profile = Profile(first_name=first_name, middle_name=middle_name, last_name=last_name, gender=gender, image=image, country=country, nationality=nationality, passport_number=passport_number, home_address=home_address, current_address=current_address, tel_number_home=tel_number_home, tel_number_mobile=tel_number_mobile, email=email, name_of_institution=name_of_institution, address_of_institution=address_of_institution, degree=degree, department=department, major=major, tem_number_of_institution=tem_number_of_institution, email_of_institution=email_of_institution, english=english, korean=korean, statement_of_interest=statement_of_interest)
+        profile.save()
+        return redirect('main')
     return render(request, 'form.html')
