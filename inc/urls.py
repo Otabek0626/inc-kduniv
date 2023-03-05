937a2c68fcf4a5120ca309e11d2bc3cdd92d2319
Otabek <otabek@Otabek-MacBook-Pro.local>
2023-03-05 17:28:03 +0900

    fix

--- inc/urls.py
+++ inc/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin', admin.site.urls),
     path('', include('index.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
