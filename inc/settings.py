e533aa076e5978adb9bfb7833abf53cf47b966c5
Otabek <otabek@Otabek-MacBook-Pro.local>
2023-03-05 17:33:51 +0900

    fix

--- inc/settings.py
+++ inc/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)pe3a%d#-3+$9ne^ag(fgng((g*p!wa7m(^sq0*fjxt=c_4s)g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
